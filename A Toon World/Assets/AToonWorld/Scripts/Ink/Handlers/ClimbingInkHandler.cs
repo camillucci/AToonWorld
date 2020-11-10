83db7a9a0b26628865580681cb09700e3a770ab6
Camillucci <nicola.camillucci@mail.polimi.it>
2020-11-10 21:23:38 +0100

    Added raycasts for vines outside borders

--- A Toon World/Assets/AToonWorld/Scripts/Ink/Handlers/ClimbingInkHandler.cs
+++ A Toon World/Assets/AToonWorld/Scripts/Ink/Handlers/ClimbingInkHandler.cs
@@ -7,6 +7,8 @@ public class ClimbingInkHandler : IInkHandler
 {
     private PlayerInkController _playerInkController;
     private bool _isDrawing = false;
+    private const float _sensibilty = 0.5f;
+    private const float _distanceFromBorder = 0.05f;
     private Vector2 _lastPoint;
 
     public ClimbingInkHandler(PlayerInkController playerInkController)
@@ -17,14 +19,17 @@ public class ClimbingInkHandler : IInkHandler
     public void OnDrawDown(Vector2 mouseWorldPosition, DrawSplineController splineController)
     {
         // First point must be on the boarder of a Ground object
-        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPosition, Vector2.zero, 500, LayerMask.GetMask(UnityTag.Default));
+        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPosition, Vector2.zero, LayerMask.GetMask(UnityTag.Default));
+        if(!hit) hit = Physics2D.Raycast(mouseWorldPosition + new Vector2(_sensibilty, 0), Vector2.zero, LayerMask.GetMask(UnityTag.Default));
+        if(!hit) hit = Physics2D.Raycast(mouseWorldPosition + new Vector2(-_sensibilty, 0), Vector2.zero, LayerMask.GetMask(UnityTag.Default));
+        
         if (hit && hit.collider.gameObject.CompareTag(UnityTag.Ground)) {
             _isDrawing = true;
             Bounds wallBounds = hit.collider.bounds;
-            if (Mathf.Abs(wallBounds.min.x - mouseWorldPosition.x) < 0.5f)
-                _lastPoint = new Vector2(wallBounds.min.x - 0.1f, mouseWorldPosition.y);
-            else if (Mathf.Abs(wallBounds.max.x - mouseWorldPosition.x) < 0.5f)
-                _lastPoint = new Vector2(wallBounds.max.x + 0.1f, mouseWorldPosition.y);
+            if (Mathf.Abs(wallBounds.min.x - mouseWorldPosition.x) < _sensibilty)
+                _lastPoint = new Vector2(wallBounds.min.x - _distanceFromBorder, mouseWorldPosition.y);
+            else if (Mathf.Abs(wallBounds.max.x - mouseWorldPosition.x) < _sensibilty)
+                _lastPoint = new Vector2(wallBounds.max.x + _distanceFromBorder, mouseWorldPosition.y);
             else
                 _lastPoint = mouseWorldPosition;
             splineController.Clear();
