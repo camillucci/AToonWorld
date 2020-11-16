1162eadfe7afedc85eaa62b12a11a01b9c6e759d
Mirko <mirko.andena@gmail.com>
2020-11-16 11:55:51 +0100

    Implementazione salto del Jumper

--- A Toon World/Assets/AToonWorld/Scripts/Enemies/JumperController.cs
+++ A Toon World/Assets/AToonWorld/Scripts/Enemies/JumperController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.AToonWorld.Scripts;
 using UnityEngine;
 
 public interface IJump
@@ -9,39 +10,46 @@ public interface IJump
 
 public class JumperController : MonoBehaviour
 {
+    [SerializeField] private float JumpHeight = 1f;
+    [SerializeField] private float JumpToY = 1f;
+    [SerializeField] private bool UseJumpHeight = false;
     [SerializeField] private float SecondsIntoDarkLake = 2f; //Seconds before next jump
     [SerializeField] private float SecondsBeforeFirstJump = 2f;
-    private bool _jump;
-    private IJump _jumpController;
+    
+    private float _jumpVelocity;
+    private bool _doneFirstJump;
+    private Rigidbody2D _rigidBody;
     
     void Start()
     {
-        _jump = false;
-        _jumpController = GetComponent<IJump>();
-        StartCoroutine(EnableFirstJump());
+        _doneFirstJump = false;
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private float CalculateVelocity(float height) => Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * height);
+
+    private void StartFirstJumpSession()
     {
-        if (_jump)
+        _jumpVelocity = CalculateVelocity(UseJumpHeight ? JumpHeight : JumpToY - transform.position.y);
+        StartCoroutine(Jump(SecondsBeforeFirstJump));
+        _doneFirstJump = true;
+    }
+
+    IEnumerator Jump(float secondsToWait)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        _rigidBody.AddForce(_jumpVelocity * Vector2.up, ForceMode2D.Impulse);
+        yield return null;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag(UnityTag.DarkLakeFloor))
         {
-            _jump = false;
-            _jumpController.Jump();
-            StartCoroutine(EnableNextJump());
+            if (_doneFirstJump)
+                StartCoroutine(Jump(SecondsIntoDarkLake));
+            else
+                StartFirstJumpSession();
         }
-    }
-
-    IEnumerator EnableFirstJump()
-    {
-        yield return new WaitForSeconds(SecondsBeforeFirstJump);
-        _jump = true;
-        yield return null;
-    }
-
-    IEnumerator EnableNextJump()
-    {
-        yield return new WaitForSeconds(SecondsIntoDarkLake);
-        _jump = true;
-        yield return null;
     }
 }
