7c32ff2cd401dd470f7d9ebac84855e45181fc34
fede.alterio <federico.alterio@mail.polimi.it>
2020-11-18 12:23:39 +0100

    Breaker can break construction ink

--- A Toon World/Assets/AToonWorld/Scripts/Enemies/Breaker/BreakerBody.cs
+++ A Toon World/Assets/AToonWorld/Scripts/Enemies/Breaker/BreakerBody.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.AToonWorld.Scripts.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,47 @@ namespace Assets.AToonWorld.Scripts.Enemies.Breaker
 {
     public class BreakerBody : MonoBehaviour
     {
+        // Private Fields
+        private readonly TaggedEvent<string, Collider2D> _triggerEnter = new TaggedEvent<string, Collider2D>();
+        private readonly TaggedEvent<string, Collider2D> _triggerExit = new TaggedEvent<string, Collider2D>();
+
+
+        // Initialization
+        private void Awake()
+        {
+            TriggerEnter.SubscribeWithTag(UnityTag.Drawing, OnDrawingEnter);
+            TriggerExit.SubscribeWithTag(UnityTag.Drawing, OnDrawingExit);
+        }
+
+        
+        // Public Properties
+        public ITaggedEvent<string, Collider2D> TriggerEnter => _triggerEnter;
+        public ITaggedEvent<string, Collider2D> TriggerExit => _triggerExit;
+
+
+
+        // Unity Events
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            _triggerEnter.InvokeWithTag(collision.gameObject.tag, collision);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            _triggerExit.InvokeWithTag(collision.gameObject.tag, collision);
+        }
+
+
+
+        // Breaker Events
+        private void OnDrawingEnter(Collider2D collision)
+        {
+            collision.gameObject.SetActive(false);
+        }
+
+        private void OnDrawingExit(Collider2D collision)
+        {
+            
+        }
     }
 }
