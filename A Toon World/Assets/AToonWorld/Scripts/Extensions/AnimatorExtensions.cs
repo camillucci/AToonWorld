eca90dc4d3d55d58e988a122d05bd8901c3ec959
fede.alterio <federico.alterio@mail.polimi.it>
2020-11-29 16:42:27 +0100

    Fading Platforms fixed

---
+++ A Toon World/Assets/AToonWorld/Scripts/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.AToonWorld.Scripts.Extensions
+{
+    public static class AnimatorExtensions
+    {
+        public static void SetProperty(this Animator @this, bool val, [CallerMemberName] string propertyName = "")
+            => @this.SetBool(propertyName, val);
+
+        public static bool GetBool(this Animator @this, [CallerMemberName] string propertyName = "")
+            => @this.GetBool(propertyName);
+    }
+}
