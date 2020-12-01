c950ac382d0809dffe1ee0835ba5384dca20cb30
Jacopo Libè <jacopo.libe@gmail.com>
2020-12-01 21:17:35 +0100

    Fix last open issues with tutorial and the player ink state

---
+++ A Toon World/Assets/AToonWorld/Scripts/Level/TimeTriggerComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class TimeTriggerComponent : MonoBehaviour
+{
+    [SerializeField] private float _secondsToTrigger = 30;
+    [SerializeField] private UnityEvent _secondsPassed = null;
+
+    private CancellationTokenSource _cancellationTokenSource;
+
+    private void Awake() 
+    {
+        _cancellationTokenSource = new CancellationTokenSource();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        TriggerEventDelayed().Forget();
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    private async UniTaskVoid TriggerEventDelayed()
+    {
+        await UniTask.Delay((int)(_secondsToTrigger * 1000), false, PlayerLoopTiming.Update, _cancellationTokenSource.Token);
+        _secondsPassed?.Invoke();
+    }
+}
