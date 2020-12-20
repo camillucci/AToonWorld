af0f5ff354f75c4d27b27bf96ba1df5e2d0d2398
fede.alterio <federico.alterio@mail.polimi.it>
2020-12-21 00:01:52 +0100

    Bubble, floating  collectible

---
+++ A Toon World/Assets/AToonWorld/Scripts/Level/BubbleCollectible.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.AToonWorld.Scripts.Level
+{    
+    public class BubbleCollectible : MonoBehaviour
+    {
+        // Editor Fields
+        [SerializeField] private float _speed;
+        [SerializeField] private float _movementRadius;
+
+
+        // Private Fields
+        private Vector2 _start;
+        private Transform _transform;
+        private Vector2 _currentDestination;
+        private Collectible _collectible;
+
+
+        private void Awake()
+        {
+            _transform = transform;
+            _start = _transform.position;
+        }
+
+        private void Start()
+        {
+            _currentDestination = _start;
+            _collectible = GetComponentInChildren<Collectible>();
+            _collectible.PlayerHit += _ => gameObject.SetActive(false);
+        }
+        
+
+
+
+        // UnityEvents
+        private void Update()
+        {
+            if (Vector2.Distance(_currentDestination, _transform.position) < float.Epsilon)
+                ChangeDestination();
+
+            _transform.position = Vector2.MoveTowards(_transform.position, _currentDestination, _speed * Time.deltaTime);
+        }
+
+
+
+
+        // Private Methods
+        private void ChangeDestination()
+        {
+            var direction = Random.insideUnitCircle;
+            var distance = Random.Range(0, _movementRadius);
+
+            _currentDestination = _start +  distance * direction;
+        }
+
+
+    }
+}
