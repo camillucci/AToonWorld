283eb897437de752cf19d6be4a77039a3339bf90
Camillucci <nicola.camillucci@mail.polimi.it>
2020-12-21 17:02:42 +0100

    Removed footsteps from audio resources

--- A Toon World/Assets/AToonWorld/Scripts/Audio/SoundEffects.cs
+++ A Toon World/Assets/AToonWorld/Scripts/Audio/SoundEffects.cs
@@ -12,7 +12,6 @@ namespace Assets.AToonWorld.Scripts.Audio
         private const string BulletPath = "Bullets/";
         private const string DrawingsPath = "Drawing/";
         private const string VictoryPath = "Checkpoint-Collectible-Victory/";
-        private const string MovementPath = "Footsteps/";
         private const string CharacterMovementPath = "Underwater/";
 
 
@@ -21,10 +20,7 @@ namespace Assets.AToonWorld.Scripts.Audio
         private static List<SoundEffect> _bulletSounds;
         private static List<SoundEffect> _drawingSounds;
         private static List<SoundEffect> _checkPointsSounds;
-        private static List<SoundEffect> _horizontalMovementSounds;
-        public static List<SoundEffect> _characterMovement;
-        private static SoundEffect _leftStep;
-        private static SoundEffect _rightStep;
+        private static List<SoundEffect> _characterMovement;
         private static SoundEffect _checkPoint;
         private static SoundEffect _victory;
         public const string Pop = nameof(Pop);
@@ -41,11 +37,8 @@ namespace Assets.AToonWorld.Scripts.Audio
         public static IReadOnlyList<SoundEffect> BulletSounds => _bulletSounds ?? (_bulletSounds = GetCategory(BulletPath));
         public static IReadOnlyList<SoundEffect> DrawingSounds => _drawingSounds ?? (_drawingSounds = GetCategory(DrawingsPath));
         public static IReadOnlyList<SoundEffect> CheckpontSounds => _checkPointsSounds ?? (_checkPointsSounds = GetCategory(VictoryPath));
-        public static IReadOnlyList<SoundEffect> HorizontalMovementSounds => _horizontalMovementSounds ?? (_horizontalMovementSounds = GetCategory(MovementPath));
         public static IReadOnlyList<SoundEffect> CharacterMovement => _characterMovement ?? (_characterMovement = GetCategory(CharacterMovementPath));
 
-        public static SoundEffect LeftStep => _leftStep ?? (_leftStep = HorizontalMovementSounds.FirstOrDefault(sound => sound.name.Equals("Footsteps/Floor/Floor_step0")));
-        public static SoundEffect RightStep => _rightStep ?? (_rightStep = HorizontalMovementSounds.FirstOrDefault(sound => sound.name.Equals("Footsteps/Floor/Floor_step1")));
         public static SoundEffect CheckPoint => _checkPoint ?? (_checkPoint = CheckpontSounds.FirstOrDefault(sound => sound.name.Equals("Checkpoint-Collectible-Victory/2")));
         public static SoundEffect Victory => _victory ?? (_victory = CheckpontSounds.FirstOrDefault(sound => sound.name.Equals("Checkpoint-Collectible-Victory/1")));
 
