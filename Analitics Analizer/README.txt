c353af4553ff4234b28644bbbdbb9e4203692059
Mirko <mirko.andena@gmail.com>
2020-12-23 12:29:22 +0100

    Formatter for analitics, now are ready to import into a db

--- Analitics Analizer/README.txt
+++
@@ -1,9 +0,0 @@
-[ RUN ]
-python main.py prod 1
-
-[ ARGS ]
-1: debug # Instead of the Analitics_production.csv use the debug one
-2: {number} # Skip {number} lines from the head int the csv file
-
-[ DEBUG ]
-python main.py debug 1
--- Analitics Analizer/formatter.py
+++ Analitics Analizer/formatter.py
@@ -1,3 +1,5 @@
+import sys
+
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     """
@@ -19,3 +21,47 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total: 
         print()
+
+def ReplaceFirstWithIndex(line):
+    line = line[1:]
+    commaIndex = line.index(',')
+    return line[commaIndex:]
+
+def InsertBefore(text, line):
+    return f"\"{text}\"{line}"
+
+# START #
+
+if len(sys.argv) < 2:
+    print('Specific a csv file as parameter')
+    exit()
+
+filename = sys.argv[1]
+file = open(filename)
+
+total = sum(1 for line in file)
+file.seek(0, 0)
+
+lines = []
+
+i = 0
+for line in file:
+    withoutFirst = ReplaceFirstWithIndex(line)
+    if i == 0:
+        withId = InsertBefore("id", withoutFirst)
+    else:
+        withId = InsertBefore(str(i), withoutFirst)
+    lines.append(withId)
+    i = i + 1
+    printProgressBar(i, total, f'Parsing {filename}', f'Complete [Total: {total}]')
+
+file.close()
+
+outfilename = 'formatted.csv'
+print('saving to', outfilename)
+
+outfile = open(outfilename, 'w')
+outfile.writelines(lines)
+outfile.close()
+
+print('Complete')
